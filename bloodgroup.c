c65afe2f519568ce28a2f08a491197a2aeed1e1b
partid123 <partid.rerk@hotmail.co.th>
2020-10-20 20:41:42 +0700

    -a

--- bloodgroup.c
+++ bloodgroup.c
@@ -29,7 +29,7 @@ void main()
     else if (strcmp(G, "B") ==0)
     {
       printf("%s, B. Well, you can give blood to: B, AB.\n", Name);
-      printf("  You can receive bloodaaa from: B, O.\n");
+      printf("  You can receive blood from: B, O.\n");
     }
     else if (strcmp(G, "AB") == 0)
     {
@@ -38,10 +38,10 @@ void main()
     }
     else
     {
-      printf("%s, O. Nice! You can give blood to all: O, A, B, AB\n", Name);
+      printf("%s, O. Nice!sss You can give blood to all: O, A, B, AB\n", Name);
       printf("  But Sad! You can receive blood only from: O\n");
     }
-    printf("\nContinue (YESxxx for Yes)? ");
+    printf("\nContinue (YES for Yes)? ");
     scanf("%s", answer);
   } while (strcmp(answer, "YES") == 0);
   printf("Goodbye\n");
